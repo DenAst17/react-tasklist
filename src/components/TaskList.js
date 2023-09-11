e2679185ed13b14992a600dba05c6ea35ce2a928
Denys Astafiev <denis2004ast@gmail.com>
2023-09-11 18:07:22 +0300

    Event bug fix

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,6 @@
 import TaskShowCard from './TaskShowCard';
 import './TaskList.css'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, componentDidMount } from 'react';
 
 export default function TaskList({
   tasks,
@@ -12,7 +12,6 @@ export default function TaskList({
 
   function dragStart(e) {
     this.style.opacity = '0.4';
-    e = this;
     setDragTask(this);
   };
 
@@ -33,12 +32,12 @@ export default function TaskList({
 
   function dragDrop(e) {
     if (dragTask != this) {
-      console.log(this.innerHTML);
-      console.log(dragTask.innerHTML);
-      const html = this.innerHTML;
-      this.innerHTML = dragTask.innerHTML;
-      dragTask.innerHTML = html;
-      swapTasks(this.dataset.id, dragTask.dataset.id);
+      setTimeout(() => {
+        const html = this.innerHTML;
+        this.innerHTML = dragTask.innerHTML;
+        dragTask.innerHTML = html;
+        swapTasks(this.dataset.id, dragTask.dataset.id);
+      }, 100);
     }
     return false;
   }
@@ -52,19 +51,29 @@ export default function TaskList({
   }
 
   function addEventsDragAndDrop(el) {
-    el.addEventListener('dragstart', dragStart, false);
-    el.addEventListener('dragenter', dragEnter, false);
-    el.addEventListener('dragover', dragOver, false);
-    el.addEventListener('dragleave', dragLeave, false);
-    el.addEventListener('drop', dragDrop, false);
-    el.addEventListener('dragend', dragEnd, false);
+    el.addEventListener('dragstart', dragStart, true);
+    el.addEventListener('dragenter', dragEnter, true);
+    el.addEventListener('dragover', dragOver, true);
+    el.addEventListener('dragleave', dragLeave, true);
+    el.addEventListener('drop', dragDrop, true);
+    el.addEventListener('dragend', dragEnd, true);
+  }
+  function removeEventsDragAndDrop(el) {
+    el.removeEventListener('dragstart', dragStart, true);
+    el.removeEventListener('dragenter', dragEnter, true);
+    el.removeEventListener('dragover', dragOver, true);
+    el.removeEventListener('dragleave', dragLeave, true);
+    el.removeEventListener('drop', dragDrop, true);
+    el.removeEventListener('dragend', dragEnd, true);
   }
   useEffect(() => {
-    var listItens = document.querySelectorAll('.draggable');
-    [].forEach.call(listItens, function (item) {
+    var listItems = document.querySelectorAll('.draggable');
+    [].forEach.call(listItems, function (item) {
+      removeEventsDragAndDrop(item);
       addEventsDragAndDrop(item);
     });
-  })
+  });
+  
   return (
     <ol className='taskList'>
       {tasks.map(task => (
